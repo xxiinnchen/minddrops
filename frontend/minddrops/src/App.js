e19b3e588f9ea5138878ff459472e7bb3c206dd3
Xin Chen <xc459@cornell.edu>
2021-04-29 12:38:57 -0400

    Test simplified app for aws

--- frontend/minddrops/src/App.js
+++ frontend/minddrops/src/App.js
@@ -85,18 +85,42 @@ function SignedInComponent() {
   )
 }
 
+// function App() {
+//   return (
+//     <React.Fragment>
+//       <Router>
+//         <Switch>
+//           {routes.map((route, idx) => (
+//             <Route path={route.path} component={route.component} key={idx} />
+//           ))}
+//         </Switch>
+//       </Router>
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
 function App() {
   return (
-    <React.Fragment>
-      <Router>
-        <Switch>
-          {routes.map((route, idx) => (
-            <Route path={route.path} component={route.component} key={idx} />
-          ))}
-        </Switch>
-      </Router>
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-// export default App;
+export default App;
+
